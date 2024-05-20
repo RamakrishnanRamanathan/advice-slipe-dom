f70e4ebcba865fcac04f1f27b8967428b85dea50
RamakrishnanRamanathan <ramakrishnanramanathan2003@gmail.com>
2024-05-20 21:46:20 +0530

    feat:working with api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
-// Write your javascript code here
+const blockQuoute=document.querySelector('blockquote');
+const refreshButton=document.querySelector('button');
+
+
+//https://api.adviceslip.com/advice//
+const URL='https://api.adviceslip.com/advice';
+//ToDo:1.Get API using Fetch method
+
+async function getApiData(){
+    try {
+        const fetchResponse= await fetch(URL);
+        const result= await fetchResponse.json();
+       return result
+    } catch (error) {
+        console.error(error);
+    }
+}
+const finalResult= await getApiData();
+blockQuoute.innerText=finalResult.slip.advice;
+refreshButton.addEventListener('click',()=>{
+    location.href='/';
+})
